d143616985315ae85232f2339a047f33a26e6134
Mental Type Design <mentallydesigned@gmail.com>
2020-10-13 15:57:33 -0700

    created size values by code

--- type/bass/scripts/sample.js
+++ type/bass/scripts/sample.js
@@ -17,11 +17,7 @@ var SampleSection = (function() {
       'Thin', 'ExtraLight', 'Light', 'Regular', 'Medium',
       'SemiBold', 'Bold'
       ],
-    sizes: [
-      8, 10, 12, 14, 16,
-      18, 22, 26, 30, 36,
-      42, 48, 56, 64, 80,
-      96, 120, 144],
+    sizes: sizes(8, 200, 50),
     letterSpacings: range(0, 0.5, 51),
     lineHeights: range(1.0, 2.0, 101),
     alignments: ['left-align', 'center', 'right-align']
@@ -87,8 +83,14 @@ var SampleSection = (function() {
   /* =============== utility methods =============== */
   function range(start, stop, count) {
     var rng = stop - start;
-    var step = rng / count;
-    return [...Array(count+1).keys()].map(i => start + i * step);
+    var step = rng / (count-1);
+    return [...Array(count).keys()].map(i => start + i * step);
+  }
+
+  function sizes(start, stop, count) {
+    var lgArray = range(Math.log2(start), Math.log2(stop), count);
+    var outArray = lgArray.map(i => 2 ** i);
+    return outArray;
   }
 
   /* =============== export public methods =============== */
